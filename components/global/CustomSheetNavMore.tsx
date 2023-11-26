7fb1bae0a5a036f75a9ca4f09765f74972aaabd1
tchisama <tchisama41@gmail.com>
2023-11-26 16:43:03 +0100

    makeing the sticker cstom sheet card here

--- components/global/CustomSheetNavMore.tsx
+++ components/global/CustomSheetNavMore.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import { Card } from '../ui/card'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+  } from "@/components/ui/popover"
+import { Slider } from '../ui/slider'
 
 type Props = {}
 
@@ -13,9 +22,20 @@ export default function CustomSheetNavMore({}: Props) {
             {
                 new Array(20).fill(0).map((_,i) => 
                 (
-                <Card key={i} className='text-gray-500 text-lg p-2 flex gap-4'>
+                <Card key={i} className='text-gray-500 text-lg p-2 flex gap-4 items-center'>
                     <img className='h-16 aspect-square rounded' src='http://localhost:3000/_next/image?url=https%3A%2F%2Fstorage.googleapis.com%2Fstickify-storage%2Fstickers%2Fva9zm4ig42a-mvtckomg5f-tkx1ygz5rwe.png&w=256&q=75'></img>
-                    {i+1}
+                    <div className='flex flex-1'>
+                        <Popover>
+                            <PopoverTrigger>
+                                <Button variant={"ghost"}>15 cm</Button>
+                            </PopoverTrigger>
+                            <PopoverContent>
+                                <Slider step={1} min={2} max={20}/>
+                            </PopoverContent>
+                        </Popover>
+                        <Button variant={"ghost"}>die cut</Button>
+                        <Button className='ml-auto' variant={"outline"}>8</Button>
+                    </div>
                 </Card>
                 )
                 )
