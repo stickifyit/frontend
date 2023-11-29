714c958e3270e201c79842f0892c8c353b7d7f59
tchisama <tchisama41@gmail.com>
2023-11-29 08:27:45 +0100

    hero images done

--- app/(client)/services/[service]/page.tsx
+++ app/(client)/services/[service]/page.tsx
@@ -5,25 +5,37 @@ import React from 'react'
 import {ArrowRight} from "lucide-react"
 import { getProductsByService } from '@/constant/allProductControlers'
 import { useParams } from 'next/navigation'
-
+import stickersHero from "@/public/product pages images/stickers hero.png"
+import tshirtHero from "@/public/product pages images/t-shirt hero.png"
 type Props = {}
+
+
+const heroImages = {
+  "stickers":stickersHero,
+  "t-shirts":tshirtHero
+}
 
 const Page = (props: Props) => {
   const params = useParams()
   return (
     <div className='min-h-screen'>
-      <div className='bg-secondary py-12 '>
-          <div className='container mx-auto space-y-4'>
-            <h1 className=' text-5xl'>Custom {params?.service}</h1>
-            <p className='max-w-5xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere dolorum recusandae voluptates dolor quas ab at dignissimos sint pariatur ut, voluptatum libero, quam minus illo repudiandae similique laudantium vel quasi.</p>
+      <div className='bg- flex gap-4'>
+          <div className='container flex gap-12 items-center mx-auto space-y-4'>
+          <div className=''>
+                 {/* i want to take the service and show image of it  */}
+                <Image width={450} src={heroImages[params?.service as 'stickers'|'t-shirts' ]??''} alt=""></Image>
           </div>
-          <div></div>
+            <div className='space-y-4'>
+              <h1 className=' text-5xl'>Custom {params?.service}</h1>
+              <p className='max-w-2xl opacity-60'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere dolorum recusandae voluptates dolor quas ab at dignissimos sint pariatur ut, voluptatum libero, quam minus illo repudiandae similique laudantium vel quasi.</p>
+            </div>
+          </div>
       </div>
       <div className='container mx-auto grid grid-cols-5 gap-6 p-6'>
           {
             // bumper , circle , die-cut , oval , rect , rounded , sheets , square
             getProductsByService(params?.service as string)?.map((item, index) => (
-              <Link href={item.href} key={index} className=' p-4 rounded-2xl border bg-gray-100 flex flex-col items-center justify-center hover:bg-secondary hover:scale-105 duration-300'>
+              <Link href={item.href} key={index} className=' p-4 rounded-2xl border bg-gray-100 flex flex-col items-center justify-center hover:bg-slate-200 hover:scale-105 duration-300'>
                 <Image src={item.img} alt={item.name} width={250}/>
                 <h3 className='text-xl text-center'>{item.name}</h3>
               </Link>
