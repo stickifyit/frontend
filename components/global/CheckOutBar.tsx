7a3381660cc41c8f5a5b2f3dbe79334ed783f679
tchisama <tchisama41@gmail.com>
2023-12-02 06:33:19 +0100

    all good

--- components/global/CheckOutBar.tsx
+++ components/global/CheckOutBar.tsx
@@ -50,32 +50,35 @@ function CheckOutBar({}: Props) {
         <div className="p-3 flex mb-4 justify-between items-center text-xl mt-10 z-20 shadow-xl shadow-[#33333307] sticky top-0 bg-white border-b">
             <h2 className='capitalize'>About order</h2>
         </div>
-        <div className='p-3 space-y-4'>
-            <h2 className='text-5xl'>Yaay!</h2>
-            <p className=' opacity-75 text-lg'>what a sheet ,<br/> you can now add it to your cart</p>
+        {
+          sheet.length > 0 &&
+            <div className='p-3 space-y-4'>
+                <h2 className='text-5xl'>Yaay!</h2>
+                <p className=' opacity-75 text-lg'>what a sheet ,<br/> you can now add it to your cart</p>
 
-              <RadioGroup value={String(sheetQuantity)} onValueChange={e=>setSheetQuantity(Number(e))}>
-                {
-                new Array(5).fill(0)
-                .map((_,q) => (
-                  <div key={q} className="flex items-center space-x-2">
-                    <RadioGroupItem value={`${q+1}`} id={`option-${q+1}`} />
-                    <Label className="flex w-full" htmlFor={`option-${q+1}`}>
-                      <div className="flex-[1]">{q+1} Sheet</div>
-                      <div className="flex-[1]">{(q+1) * 25}Dh</div>
-                      {/* <div className="text-green-700 flex-[1] justify-end flex">
-                        {14}%
-                      </div> */}
-                    </Label>
-                  </div>
-                ))}
-              </RadioGroup>
+                  <RadioGroup value={String(sheetQuantity)} onValueChange={e=>setSheetQuantity(Number(e))}>
+                    {
+                    new Array(5).fill(0)
+                    .map((_,q) => (
+                      <div key={q} className="flex items-center space-x-2">
+                        <RadioGroupItem value={`${q+1}`} id={`option-${q+1}`} />
+                        <Label className="flex w-full" htmlFor={`option-${q+1}`}>
+                          <div className="flex-[1]">{q+1} Sheet</div>
+                          <div className="flex-[1]">{(q+1) * 25}Dh</div>
+                          {/* <div className="text-green-700 flex-[1] justify-end flex">
+                            {14}%
+                          </div> */}
+                        </Label>
+                      </div>
+                    ))}
+                  </RadioGroup>
 
-            <div className='flex items-center gap-2 justify-between'>
-                <h2 className='text-2xl capitalize'>{sheetQuantity*25} dh</h2>
-                <Button onClick={handleAddToCart} disabled={!(sheet.length>0)} className='sticky bottom-0' size="lg">Add to cart <ShoppingBasket/></Button>
+                <div className='flex items-center gap-2 justify-between'>
+                    <h2 className='text-2xl capitalize'>{sheetQuantity*25} dh</h2>
+                    <Button onClick={handleAddToCart} disabled={!(sheet.length>0)} className='sticky bottom-0' size="lg">Add to cart <ShoppingBasket/></Button>
+                </div>
             </div>
-        </div>
+        }
     </div>
 
   )
