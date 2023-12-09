1792568ae98e72f309f8ad52182663cf87f13b9a
tchisama <tchisama41@gmail.com>
2023-12-09 15:05:20 +0100

    all good again

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
+      
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
